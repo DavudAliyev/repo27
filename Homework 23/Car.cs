4241fc13dbb5fc90508b6e184c0199eef7cff9c9
Davud <davud.aliyev2007@gmail.com>
2024-01-31 18:12:39 +0400

    Add project files.

---
+++ Homework 23/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_23
+{
+    internal class Car
+    {
+        public string Model;
+        public string Brand;
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(Model + " - " + Brand);
+        }
+    }
+}
---
+++ Homework 23/Program.cs
@@ -0,0 +1,20 @@
+﻿using Homework_23;
+
+string[] names = new string[5];
+
+StringList namelist= new StringList();
+
+namelist.Add("Sahin");
+namelist.Add("Raul");
+namelist.Add("Rauf");
+namelist.Add("Aytac");
+
+
+for(int i=0; i<namelist.Length; i++)
+{
+    Console.WriteLine(namelist[i]);
+}
+
+MyList<Car> list = new MyList<Car>();
+
+list.Add(new Car());
